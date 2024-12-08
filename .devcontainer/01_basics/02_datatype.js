fa1a5f161241f981a96f1acb3ed5ede1aa089db0
Hamdan Hussain <hamdanhussain888999@gmail.com>
2024-12-08 14:57:40 +0500

    Add files via upload

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as new version
+
+// alert("your answer is :")
+// console.log( 3 + 3)
+
+let a = null;
+let b = 12345;
+let c = Symbol("i'm a synbol")
+let d = "hamdan"
+let e = true;
+let f = BigInt(321) + BigInt(4)
+let g = undefined
+console.log(a, b, c, d, e, f, g)
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f, typeof g, typeof h)
