c5b208bdf4eb1a81f0060943f0a8e39d5e42e6af
Hamdan Hussain <hamdanhussain888999@gmail.com>
2024-12-08 14:57:08 +0500

    Add files via upload

---
+++ .devcontainer/01_basics/01_varibale.js
@@ -0,0 +1,9 @@
+// console.log('ta wali rafulyadin kawi?')
+// console.log("Hello Words")
+let accountname;
+const accountId = 5678
+let accountmail = "gryaygg5654@gmail.com"
+var accountpassword = "98789"
+accountaddres = "ouch"
+
+console.log([accountId, accountmail, accountpassword, accountaddres, accountname]);
