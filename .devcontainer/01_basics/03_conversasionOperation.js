81740d22108391bab0ac18cb558fbf15f92ded4c
Hamdan Hussain <hamdanhussain888999@gmail.com>
2024-12-11 22:19:00 +0500

    Add files via upload

---
+++ .devcontainer/01_basics/03_conversasionOperation.js
@@ -0,0 +1,42 @@
+// let age= 20
+// console.log(typeof age);
+// console.log(typeof(age));
+
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let logIn = 1
+// let booleanLogIn = Boolean(logIn)
+// console.log(booleanLogIn)
+
+// let somenumber = 55
+// let StringNumber = String(somenumber)
+// console.log(StringNumber)
+
+// opeararions
+
+// let value = 6
+// let negtValue = -value
+// console.log(negtValue)
+
+// console.log('Opretor in js')
+// let a = 25;
+// let b = 5;
+// console.log('a + b = ', a+b)
+// console.log('a - b = ', a-b)
+// console.log('a * b = ', a*b)
+// console.log('a / b = ', a/b)
+// console.log('a % b = ', a%b)
+// console.log('a ** b = ', a**b)
+// console.log('a++ = ', a++)
+// console.log('a-- = ', a--)
+// console.log('a === b = ', a===b)
+// console.log('a !== b = ', a!==b)
+
+// let str1 = "Hello"
+// let str2 = " Hamdan "
+// let str3 = str1 + str2
+// console.log(str3)
+
+console.log(+true)
